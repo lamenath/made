09b337a702eef576d405e5be224bf90cae69c5cc
Sebastien Renault <srenault.contact@gmail.com>
2016-06-27 11:45:25 +0200

    Initial commit

---
+++ prismic-configuration.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  apiEndpoint: 'https://made-demo.prismic.io/api',
+
+  // This function will be used to generate links to Prismic.io documents
+  // As your project grows, you should update this function according to your routes
+  linkResolver: function(doc, ctx) {
+    if (doc.type == 'page') {
+      return '/' + doc.uid;
+    }
+    return '/';
+  }
+};
