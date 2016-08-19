090a91278a78dd33d9a690a1b31e51c88fd68feb
Nathanael Lamelliere <nla@zenexity.com>
2016-08-19 11:05:28 +0200

    Update prismic-configuration.js

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  apiEndpoint: 'https://[YOUR-REPOSITORY].prismic.io/api',
+  apiEndpoint: 'https://made-demo.prismic.io/api',
 
   // This function will be used to generate links to Prismic.io documents
   // As your project grows, you should update this function according to your routes
